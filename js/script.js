616247f64b6985af3a72cfb54a3abc0de01838f5
Riyad <saifulbangali@gmail.com>
2022-09-09 22:17:19 +0600

    Project upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function($){
+    //lightbox js
+    new VenoBox({
+        selector: ".venobox",
+        titleattr: 'data-title',
+        titlePosition: 'top',
+        spinner: "swing",
+        spinColor: "#fec600",
+        share: "true",
+        numeration: "true",
+        infinigall: "true",
+    });
+});
